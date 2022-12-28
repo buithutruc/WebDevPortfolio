41489ec94ef65887c6bc64294724c13cea2ac9e5
buithutruc <112782993+buithutruc@users.noreply.github.com>
2022-12-28 17:56:07 -0500

    added validation for personal info & payment

--- chez_moi_hotel/scripts/booking.js
+++ chez_moi_hotel/scripts/booking.js
@@ -58,3 +58,49 @@ function calculateDates() {
       Difference_In_Days
   );
 }
+
+function validateForm() {
+  let first_name = document.forms["reservation"]["first_name"].value;
+  let last_name = document.forms["reservation"]["last_name"].value;
+  let address_line1 = document.forms["reservation"]["address_line1"].value;
+  let address_line2 = document.forms["reservation"]["address_line2"].value;
+  let town = document.forms["reservation"]["town"].value;
+  let zip = document.forms["reservation"]["zip"].value;
+  let radios = document.querySelectorAll('input[type="radio"]:checked');
+  let selected_card = radios.length > 0 ? radios[0].value : null;
+  let name_on_card = document.forms["reservation"]["cname"].value;
+  let card_number = document.forms["reservation"]["cnumber"].value;
+
+  if (first_name == "") {
+    alert("Please enter your first name!");
+    return false;
+  } else if (last_name == "") {
+    alert("Please enter your last name!");
+    return false;
+  } else if (address_line1 == "") {
+    alert("Please enter your address!");
+    return false;
+  } else if (address_line2 == "") {
+    alert("Please enter your address!");
+    return false;
+  } else if (town == "") {
+    alert("Please enter your town!");
+    return false;
+  } else if (zip == "") {
+    alert("Please enter your postal code!");
+    return false;
+  } else if (selected_card == null) {
+    alert("Please select a card type !");
+    return false;
+  } else if (name_on_card == "") {
+    alert("Please enter the name on card!");
+    return false;
+  } else if (card_number == null) {
+    alert("Please enter card number!");
+    return false;
+  } else {
+    var text = "Thank you " + first_name + ". Your booking is placed!";
+    alert(text);
+    return true;
+  }
+}
