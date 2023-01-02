e4501ae57cd52027a0c8e59baac0c7f7b6c8829b
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-02 14:55:48 -0500

    added comments to js files

--- chez_moi_hotel/scripts/booking.js
+++ chez_moi_hotel/scripts/booking.js
@@ -1,64 +1,64 @@
-window.addEventListener("load", () => {
-  //debugger;
-  // Via Query parameters - GET
-  const params = new URL(document.location).searchParams;
+receiveResult();
 
-  const checkin = params.get("checkin-date");
+/**
+ * receive the checkin and checkout dates from the homepage
+ */
+function receiveResult() {
+  window.addEventListener("load", () => {
+    //debugger;
+    // Via Query parameters - GET
+    const params = new URL(document.location).searchParams;
+    const checkin = params.get("checkin-date");
+    const checkout = params.get("checkout-date");
 
-  const checkout = params.get("checkout-date");
+    // const price = params.get("price");
+    const price = sessionStorage.getItem("Price");
+    console.log(price);
+    //   debugger;
 
-  // const price = params.get("price");
-  const price = sessionStorage.getItem("Price");
-  console.log(price);
-  //   debugger;
+    let total;
 
-  let total;
+    // Via local Storage
+    document.getElementById("result-checkindate").innerHTML = checkin;
+    document.getElementById("result-checkoutdate").innerHTML = checkout;
+    calculateDates();
 
-  // Via local Storage
-  document.getElementById("result-checkindate").innerHTML = checkin;
+    document.getElementById("result-price").innerText = "$" + price;
+    total =
+      parseFloat(price) *
+      parseFloat(document.getElementById("total-stay").innerText);
+    document.getElementById("total").innerText = "$" + total.toFixed(2);
 
-  document.getElementById("result-checkoutdate").innerHTML = checkout;
-  calculateDates();
-
-  document.getElementById("result-price").innerText = "$" + price;
-  total =
-    parseFloat(price) *
-    parseFloat(document.getElementById("total-stay").innerText);
-  document.getElementById("total").innerText = "$" + total.toFixed(2);
-
-  //   debugger;
-});
-
+    //   debugger;
+  });
+}
+/**
+ * calculate the number of days difference between two dates
+ */
 function calculateDates() {
-  // JavaScript program to illustrate
-  // calculation of no. of days between two date
-
   // To set two dates to two variables
   var str1 = document.getElementById("result-checkindate").innerText;
   let date1 = new Date(str1);
   console.log(date1);
+
   var str2 = document.getElementById("result-checkoutdate").innerText;
   let date2 = new Date(str2);
   console.log(date2);
 
-  // To calculate the time difference of two dates
+  // calculate the time difference of two dates
   var Difference_In_Time = date2.getTime() - date1.getTime();
 
-  // To calculate the no. of days between two dates
+  // calculate the no. of days between two dates
   var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
 
+  // display the final no. of days (result)
   document.getElementById("total-stay").innerHTML = Difference_In_Days;
-  //To display the final no. of days (result)
-  console.log(
-    "Total number of days between dates <br>" +
-      date1 +
-      "<br> and <br>" +
-      date2 +
-      " is: <br> " +
-      Difference_In_Days
-  );
 }
 
+/**
+ * validate if all the required fields in the form are filled
+ * @returns boolean
+ */
 function validateForm() {
   let first_name = document.forms["reservation"]["first_name"].value;
   let last_name = document.forms["reservation"]["last_name"].value;
