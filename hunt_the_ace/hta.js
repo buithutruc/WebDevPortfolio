a84dbb16bb70eb989a408ddefa1b0526fa4bfaa7
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-11 19:11:04 -0500

    working on js creating cards dynamically

--- hunt_the_ace/hta.js
+++ hunt_the_ace/hta.js
@@ -5,8 +5,11 @@ const cardObjectDefinitions = [
   { id: 4, imagePath: "/hunt_the_ace/images/card-AceSpades.png" },
 ];
 
-{
-  /* <div class="card">
+const cardBackImgPath = "/hunt_the_ace/images/card-back-Blue.png";
+
+const cardContainerElem = document.querySelector(".card-container");
+
+/* <div class="card">
           <div class="card-inner">
             <div class="card-front">
               <img
@@ -24,7 +27,6 @@ const cardObjectDefinitions = [
             </div>
           </div>
         </div> */
-}
 
 function createCard(cardItem) {
   //create div elements that make up a card
@@ -36,8 +38,64 @@ function createCard(cardItem) {
   //create front and back image elements for a card
   const cardFrontImg = createElement("img");
   const cardBackImg = createElement("img");
+
+  //add class to card element
+  addClassToElement(cardElem, "card");
+  addIdToElement(cardElem, cardItem.id);
+
+  //add class to inner card element
+  addClassToElement(cardInnerElem, "card-inner");
+
+  //add class to front card element
+  addClassToElement(cardFrontElem, "card-front");
+
+  //add class to back card element
+  addClassToElement(cardBackElem, "card-back");
+
+  //add src attribute and appropriate value to img element - back of card
+  addSrcToImageElem(cardBackElem, cardBackImgPath);
+
+  //add src attribute and appropriate value to img element - front of card
+  addSrcToImageElem(cardFrontElem, cardItem.imagePath);
+
+  //assign class to back image element of back of card
+  addClassToElement(cardBackElem, "card-img");
+
+  //assign class to front image element of front of card
+  addClassToElement(cardBackElem, "card-img");
+
+  //add front image element as child element to front card element
+  addChildElement(cardFrontElem, cardFrontImg);
+
+  //add back image element as child element to back card element
+  addChildElement(cardBackElem, cardBackImg);
+
+  //add front image element as child element to inner card element
+  addChildElement(cardInnerElem, cardFrontElem);
+
+  //add back image element as child element to inner card element
+  addChildElement(cardInnerElem, cardBackElem);
+
+  //add inner card element as child element to card element
+  addChildElement(cardElem, cardInnerElem);
 }
 
 function createElement(elemType) {
   return document.createElement(elemType);
+}
+
+function addClassToElement(elem, className) {
+  elem.classList.add(className);
+}
+
+function addIdToElement(elem, id) {
+  elem.id = id;
+}
+
+function addSrcToImageElem(imgElem, src) {
+  imgElem.src = src;
+}
+
+function addChildElement(parentElem, childElem) {
+  parentElem.appendChild(childElem);
 }
